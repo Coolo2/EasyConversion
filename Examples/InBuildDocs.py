96ed14ebd1295bae19b1655e26c1045230ec51e8
Coolo22 <39219877+Coolo22@users.noreply.github.com>
2020-06-28 19:18:18 +0100

    Create InBuildDocs.py

---
+++ Examples/InBuildDocs.py
@@ -0,0 +1,12 @@
+from EasyConversion import docs
+#imports
+
+
+print(docs.decimal.letter)
+#Prints docs for converting decimal to letter
+
+
+binary_docs = docs.binary.decimal
+print(binary_docs)
+#Creates variable binary_docs with docs for binary to decimal conversions
+#Prints the variable
