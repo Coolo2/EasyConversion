6ed658b568e4a6828543a4774c21f20e4b821a8b
Coolo22 <39219877+Coolo22@users.noreply.github.com>
2020-06-28 19:29:24 +0100

    Create Info.py

---
+++ Examples/Info.py
@@ -0,0 +1,13 @@
+from EasyConversion import info
+#Imports
+
+print("We are version " + info.version.name)
+#Print the version number
+#Outputs:
+#   We are version 0.4.1 [replace 0.4.1 with current version]
+
+
+print("Version " + info.version.name + " released on " + info.version.release_date + ".")
+#Prints the version name with release date
+#Outputs:
+#   Version 0.4.1 released on June 28th 2020 [these are replaced with correct values]
