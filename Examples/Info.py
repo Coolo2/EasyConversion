c3cbff5c4380b7d9d0eca045773bfe2810e04714
Coolo22 <39219877+Coolo22@users.noreply.github.com>
2020-06-28 19:37:07 +0100

    Update Info.py

--- Examples/Info.py
+++ Examples/Info.py
@@ -7,7 +7,7 @@ print("We are version " + info.version.name)
 #   We are version 0.4.1 [replace 0.4.1 with current version]
 
 
-print("Version " + info.version.name + " released on " + info.version.release_date + ".")
+print("Version " + info.version.name + " was released on " + info.version.release_date + ".")
 #Prints the version name with release date
 #Outputs:
 #   Version 0.4.1 released on June 28th 2020 [these are replaced with correct values]
