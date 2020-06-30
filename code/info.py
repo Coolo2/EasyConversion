fbd397937f843e721bd4a9baa93daa58084d6804
Coolo22 <39219877+Coolo22@users.noreply.github.com>
2020-06-30 16:25:38 +0100

    Update info.py

--- code/info.py
+++ code/info.py
@@ -1,3 +1,30 @@
 class version:
-    name = "0.4.0"
-    release_date = "28th June 2020"
+    class current:
+        name = "0.5.0"
+        release_date = "29th June 2020"
+
+    class get_version:
+        def __init__(self, search):
+            if search == "0.5.0":
+                self.name = "0.5.0"
+                self.release_date = "29 June 2020"
+            elif search == "0.4.1":
+                self.name = "0.4.1"
+                self.release_date = "28 June 2020"
+            elif search == "0.5.0":
+                self.name = "0.5.0"
+                self.release_date = "28 June 2020"
+            elif search == "0.3.1":
+                self.name = "0.3.1"
+                self.release_date = "28 June 2020"
+            elif search in ["0.3.0", "0.3"]:
+                self.name = "0.3"
+                self.release_date = "28 June 2020"
+            elif search in ["0.2", "0.2.0"]:
+                self.name = "0.2"
+                self.release_date = "27 June 2020"
+            elif search in ["0.1", "0.1.0"]:
+                self.name = "0.1"
+                self.release_date = "27 June 2020"
+            else:
+                raise Exception("Invalid release")
