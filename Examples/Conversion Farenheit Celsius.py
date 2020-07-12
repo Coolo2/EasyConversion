dc4378c845f7963528a14eac7ffff0236f2c948b
Coolo22 <39219877+Coolo22@users.noreply.github.com>
2020-07-12 13:00:31 +0100

    Create Conversion Farenheit Celsius.py

---
+++ Examples/Conversion Farenheit Celsius.py
@@ -0,0 +1,12 @@
+from EasyConversion import convert 
+#importing the convert module
+
+print(convert.detect.farenheitcelsius("50f"))
+#Detects if it is calculating from farenheit or celsius and converts
+#Returns:
+#   10.0
+
+print(convert.celsius.farenheit("100"))
+#Converts from celsius to farenheit
+#Returns:
+#   212.0
