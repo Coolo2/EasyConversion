20666391f4c86a22f8874ffee8140b7b818468a5
Coolo22 <39219877+Coolo22@users.noreply.github.com>
2020-07-12 14:15:59 +0100

    Update docs.py

--- code/docs.py
+++ code/docs.py
@@ -3,48 +3,10 @@ from EasyConversion import textformat
 colour = textformat.color
 
 class Dec:
-    Bin = f"""
-{colour.BOLD} **CONVERT DECIMAL TO BINARY** {colour.END}
-{colour.UNDERLINE}Usage:{colour.END}
-    convert.decimal.binary(decimal, return_type=str)
-    {colour.UNDERLINE}Arguments:{colour.END}
-        decimal = decimal number to input [int, str, list]
-        return_type = int, str, bin
-{colour.UNDERLINE}Input: {colour.END}
-    Input can be in a str, int, or list
-{colour.UNDERLINE}Output:{colour.END}
-    Output can be in a str, int, or [list](if input type is list) 
-    Output type defaults to bin
-    Output type can be changed with argument return_type=[str, int, bin]
-    If input type is list, it returns all sections converted in the same order
-    List form returns '0' in error.
-{colour.UNDERLINE}Aliases:{colour.END}
-    dec.Bin, Decimal.Binary, Dec.Bin, Decimal.binary
-{colour.BOLD}More advanced docs can be found at
-https://easyconversion.readthedocs.io/
-{colour.END}  
-    """
+    Bin = "https://easyconversion.readthedocs.io/en/master/#decimal-to-binary"
 
-    Letter = f"""
-{colour.BOLD} **CONVERT DECIMAL TO LETTER** {colour.END}
-Converts between decimal and letter (based on alphabet, not ascii)
-{colour.UNDERLINE}Usage:{colour.END}
-    convert.decimal.letter(decimal)
-    {colour.UNDERLINE}Arguments:{colour.END}
-        decimal = decimal number to input [int, str, list]
-        return_type = str / str-list
-{colour.UNDERLINE}Input: {colour.END}
-    Input can be in a str, int, or list
-{colour.UNDERLINE}Output:{colour.END}
-    Output can be in a str or [list](if input type is list) 
-    If input type is list, it returns all sections converted in the same order
-    List form returns None in error.
-{colour.UNDERLINE}Aliases:{colour.END}
-    dec.letter, Decimal.Letter, Dec.Let, dec.let, Decimal.letter
-{colour.BOLD}More advanced docs can be found at
-https://easyconversion.readthedocs.io/
-{colour.END}  
-    """
+    Letter = "https://easyconversion.readthedocs.io/en/master/#decimal-to-letter"
+
     letter, let, Let = Letter, Letter, Letter
     bin, binary, Binary = Bin, Bin, Bin
 
@@ -52,57 +14,32 @@ https://easyconversion.readthedocs.io/
 
 decimal, dec, Decimal = Dec, Dec, Dec
 
-
-
 class Bin():
-    Dec = f"""
-{colour.BOLD}CONVERT BINARY TO DECIMAL{colour.END}
-{colour.UNDERLINE}Usage:{colour.END}
-    convert.binary.decimal(binary, return_type=str)
-    {colour.UNDERLINE}Arguments:{colour.END}
-        binary = binary number to input [int, str, list, bin]
-        return_type = int, str
-{colour.UNDERLINE}Input: {colour.END}
-    Input can be in a str, int, bin or list (of any of the previous)
-{colour.UNDERLINE}Output:{colour.END}
-    Output can be in a str, int, or [list](if input type is list) 
-    Output type defaults to int
-    Output type can be changed with argument return_type=[str, int, bin]
-    If input type is list, it returns all sections converted in the same order
-    List form returns '0' in error.
-{colour.UNDERLINE}Aliases:{colour.END}
-    Bin.Dec, Bin.dec, Binary.Decimal, Binary.decimal
-{colour.BOLD}More advanced docs can be found at
-https://easyconversion.readthedocs.io/
-{colour.END}  
-"""
+    Dec = "https://easyconversion.readthedocs.io/en/master/#binary-to-decimal"
     dec, decimal, Decimal = Dec, Dec, Dec
 
 binary, Binary, bin = Bin, Bin, Bin
 
 class Letter:
-    Decimal = f"""
-{colour.BOLD} **CONVERT LETTER TO DECIMAL** {colour.END}
-Converts between letter and decimal (based on alphabet, not ascii)
-{colour.UNDERLINE}Usage:{colour.END}
-    convert.letter.decimal(letter, return_type=int)
-    {colour.UNDERLINE}Arguments:{colour.END}
-        letter = letter to input [str, list]
-        return_type = str / int / str/int list
-{colour.UNDERLINE}Input: {colour.END}
-    Input can be in a str or list
-{colour.UNDERLINE}Output:{colour.END}
-    Output can be in a str, int or [list](if input type is list) 
-    Output type can be determined with argument return_type=[type]
-    If input type is list, it returns all sections converted in the same order
-    List form returns None in error.
-{colour.UNDERLINE}Aliases:{colour.END}
-    letter.dec, Letter.Decimal, Let.Dec, let.dec, Letter.decimal
-{colour.BOLD}More advanced docs can be found at
-https://easyconversion.readthedocs.io/
-{colour.END}  
-    """
+    Decimal = "https://easyconversion.readthedocs.io/en/master/#letter-to-decimal"
 
     dec, Dec, decimal = Decimal, Decimal, Decimal
                                 
 letter, let, Let = Letter, Letter, Letter
+
+class String:
+    Ascii = "https://easyconversion.readthedocs.io/en/master/#letter-string-to-ascii"
+    asc, Asc, asciibinary, AsciiBinary, Asciibinary = Ascii, Ascii, Ascii, Ascii, Ascii
+    morse = "https://easyconversion.readthedocs.io/en/master/#string-to-morse"
+    Morse, morsecode, Morsecode, MorseCode = morse, morse, morse, morse
+letter, string, Letter, Str, let, Let = String, String, String, String, String, String
+
+class Ascii:
+    String = "https://easyconversion.readthedocs.io/en/master/#ascii-binary-to-letter-string"
+    letter, string, Letter, Str, let, Let = String, String, String, String, String, String
+asc, Asc, asciibinary, AsciiBinary, Asciibinary = Ascii, Ascii, Ascii, Ascii, Ascii
+
+class morse:
+    String = "https://easyconversion.readthedocs.io/en/master/#morse-to-string"
+    letter, string, Letter, Str, let, Let = String, String, String, String, String, String
+Morse, morsecode, Morsecode, MorseCode = morse, morse, morse, morse
