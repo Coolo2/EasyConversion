b7af270528313ab9aa4166339ebede7547eecf75
Coolo22 <39219877+Coolo22@users.noreply.github.com>
2020-07-27 13:54:01 +0100

    Shorten oversized line breaks

--- Examples/Conversion Binary Decimal.py
+++ Examples/Conversion Binary Decimal.py
@@ -1,18 +1,13 @@
 from EasyConversion import convert
 #Imports
 
-
-
 print(convert.binary.decimal(["10101", "1111", "11111111", "InvalidBinary"], return_type=str))
-
 #Prints the list of binaries as decimal numbers, returning in a str list.
 #Returns:
 #    ['21', '15', '255', '0']
 
-
-
 binary = convert.decimal.binary("21", return_type=bin)
-
+print(binary)
 #Converts the decimal (as a str, can be int) into binary, returning as a bin. 
 #Returns:
 #    0b10101
